1e2c8e757e1a58da14c22701274f21f81f33f654
jaimeaRios <jaimearios1986@gmail.com>
2021-10-10 21:12:50 -0500

    add fields for video schema, add upload video end point, add increase likes end point, add search endpoint

--- src/models/video.js
+++ src/models/video.js
@@ -6,6 +6,34 @@ const videoSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
+    title:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    keyWord :{
+        type: String,
+        required: true,
+        trim: true
+    },
+    likes: {
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value<0){
+                throw new Error('Number of like must be a positive number')
+            }
+        }
+    },
+    views:{
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value<0){
+                throw new Error('Number of views must be a positive number')
+            }
+        }
+    },
     data:{
         type: Buffer
     }
--- src/routers/video.js
+++ src/routers/video.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const multer = require('multer')
 const Video = require('../models/video')
 const router = new express.Router()
 
-router.get('/video', async(req, res)=>{
-    //const video = new Video(req.body);
+router.get('/video/test', async(req, res)=>{
     res.status(200).send('All is ok!');
-
 })
 
 router.post('/video', async(req,res)=>{
@@ -16,6 +15,101 @@ router.post('/video', async(req,res)=>{
         res.status(201).send({video});
     }catch(e){
         res.status(400).send(e);
+    }
+})
+
+var storage = multer.diskStorage({
+    destination: function (request, file, callback){
+        callback(null, './public/uploads');
+    }, 
+    filename: function(request, file, callback){
+        console.log(file); 
+        callback(null, file.originalname)
+    }
+}); 
+
+const upload = multer({
+    storage,
+    limits:{
+        fileSize: 100000000
+    },
+    fileFilter(req, file, cb){
+        console.log(file.originalname);
+
+        if(!file.originalname.match(/\.(mp4|vid)$/)){
+            return cb(new Error('Please upload a video'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/video/upload', upload.single('video'), async(req, res)=>{
+    console.log(req.file)
+    res.send()
+},(error, req, res, next)=>{
+    res.status(400).send({error: error.message})
+})
+
+router.patch('/video/like/:id', async(req, res)=>{
+    const like = req.body.like;
+    if(!like){
+        return res.status(400).send();
+    }
+
+    try{
+        const video = await Video.findOne({_id:req.params.id});
+        
+        if(!video){
+            return res.status(404).send();
+        }
+
+        if(like === true){
+            video.likes++;
+        }
+        else{
+            if(video.likes>0)
+                video.likes--;
+        }
+
+        await video.save();
+        res.send(video)
+        
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+router.get('/video', async(req, res)=>{
+    const title = req.body.title;
+    const keyWord = req.body.keyWord;
+    
+    if(!title && !keyWord){
+        return res.status(400).send();
+    }
+
+    try{
+        if((title && title !== "")&&(keyWord && keyWord!=="")){
+            const query = {title : {"$regex": title},keyWord: {"$regex": keyWord} }
+            const videos = await Video.find(query);
+            res.send(videos)
+        }
+        else if(title && title !== ""){
+            const query = {title : {"$regex": title} }
+            const videos = await Video.find(query);
+            res.send(videos)
+        }
+        else if(keyWord && keyWord!==""){
+            const query = {keyWord: {"$regex": keyWord} }
+            const videos = await Video.find(query);
+            res.send(videos)
+        }
+        else{
+            res.status(404).send()
+        }
+        
+    }catch(e){
+        res.status(400).send(e)
     }
 })
 module.exports = router
